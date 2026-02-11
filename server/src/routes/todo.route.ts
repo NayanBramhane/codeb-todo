9d245832eef0c848fda0b08d43fe97bb3cca6522
Nayan Bramhane <74374719+NayanBramhane@users.noreply.github.com>
2026-02-11 22:58:10 +0530

    added validations for todo and user routes

--- server/src/routes/todo.route.ts
+++ server/src/routes/todo.route.ts
@@ -6,16 +6,46 @@ import {
   updateTodo,
 } from '../controllers/todo.controller.js';
 import { isAuthenticated } from '../middleware/auth.js';
+import { body, param } from 'express-validator';
+import { validate } from '../middleware/validate.js';
 
 const router = express.Router();
 
-router.post('/new', isAuthenticated, createTodo);
+router.post(
+  '/',
+  isAuthenticated,
+  [
+    body('title')
+      .notEmpty()
+      .withMessage('Title is required')
+      .isLength({ max: 100 })
+      .withMessage('Title must be less than 100 characters'),
+
+    body('description')
+      .optional()
+      .isLength({ max: 500 })
+      .withMessage('Description too long'),
+  ],
+  validate,
+  createTodo,
+);
 
 router.get('/my', isAuthenticated, getTodo);
 
-router
-  .route('/:id')
-  .put(isAuthenticated, updateTodo)
-  .delete(isAuthenticated, deleteTodo);
+router.put(
+  '/:id',
+  isAuthenticated,
+  [param('id').isMongoId().withMessage('Invalid Todo ID')],
+  validate,
+  updateTodo,
+);
+
+router.delete(
+  '/:id',
+  isAuthenticated,
+  [param('id').isMongoId().withMessage('Invalid Todo ID')],
+  validate,
+  deleteTodo,
+);
 
 export default router;
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,13 +1,35 @@
 import { Router } from 'express';
 import { register, login, logout } from '../controllers/user.controller.js';
+import { validate } from '../middleware/validate.js';
+import { body } from 'express-validator';
 
 const router = Router();
 
 // Register new user
-router.post('/register', register);
+router.post(
+  '/register',
+  [
+    body('email').isEmail().withMessage('Valid email is required'),
+
+    body('password')
+      .isLength({ min: 6 })
+      .withMessage('Password must be at least 6 characters'),
+  ],
+  validate,
+  register,
+);
 
 // Login user
-router.post('/login', login);
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Valid email is required'),
+
+    body('password').notEmpty().withMessage('Password is required'),
+  ],
+  validate,
+  login,
+);
 
 // Logout user
 router.get('/logout', logout);
