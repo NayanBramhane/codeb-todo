c6bbb47001991ba7836ce71a74e856bd41d04068
Nayan Bramhane <74374719+NayanBramhane@users.noreply.github.com>
2026-02-12 23:27:39 +0530

    added zod schema for login

--- client/src/schemas/auth.schema.ts
+++ client/src/schemas/auth.schema.ts
@@ -1,9 +1,17 @@
 import * as z from "zod";
 
+export const loginSchema = z.object({
+  email: z.email("Please enter a valid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+// Type inference for the Login component
+export type LoginValues = z.infer<typeof loginSchema>;
+
 export const registerSchema = z
   .object({
-    email: z.email(),
-    password: z.string().min(6),
+    email: z.email("Invalid email address"), 
+    password: z.string().min(6, "Password must be at least 6 characters"),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
@@ -11,4 +19,5 @@ export const registerSchema = z
     path: ["confirmPassword"],
   });
 
+// Type inference for the Register component
 export type RegisterValues = z.infer<typeof registerSchema>;
