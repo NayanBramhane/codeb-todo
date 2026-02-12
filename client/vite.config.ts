b3092856ce41ac431735f30900b57562ea3a9477
Nayan Bramhane <74374719+NayanBramhane@users.noreply.github.com>
2026-02-12 23:26:45 +0530

    added base api to vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path"
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
@@ -9,6 +9,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
