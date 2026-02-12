5a8893c5352793b070c445e398364d7caf47eebd
Nayan Bramhane <74374719+NayanBramhane@users.noreply.github.com>
2026-02-12 23:27:06 +0530

    added routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
-import { Login } from "./components/Login";
-import Register from "./components/Register";
-
 function App() {
   return (
     <>
-      <Login />
+      App
     </>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router";
+import { Login } from "./components/Login"; 
+import Register from "./components/Register";
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Toaster position="top-center" richColors /> 
+      <Routes>
+        {/* Set Register as the index/first page */}
+        <Route path="/" element={<Register />} />
+        
+        {/* Other routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<App />} />
+
+        {/* Fallback: Redirect unknown paths to Register */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
