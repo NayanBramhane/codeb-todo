a0c45b4a7f459d18a18fdbb68882a44be4dd2441
Nayan Bramhane <74374719+NayanBramhane@users.noreply.github.com>
2026-02-12 14:02:55 +0530

    added pagination logic for getTodo, dynamically update todo fields.

--- server/src/controllers/todo.controller.ts
+++ server/src/controllers/todo.controller.ts
@@ -36,12 +36,25 @@ export const getTodo = async (
   try {
     if (!req.user) return next(new ErrorHandler('Unauthorized', 401));
 
-    const todos = await Todo.find({ owner: req.user._id }).sort({
-      createdAt: -1,
-    });
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+
+    const [todos, totalTodos] = await Promise.all([
+      Todo.find({ owner: req.user._id })
+        .sort({ createdAt: -1 })
+        .limit(limit)
+        .skip(skip),
+      Todo.countDocuments({ owner: req.user._id }),
+    ]);
+
+    const totalPages = Math.ceil(totalTodos / limit);
 
     res.status(200).json({
       success: true,
+      totalTodos,
+      totalPages,
       todos,
     });
   } catch (error) {
@@ -57,11 +70,18 @@ export const updateTodo = async (
   try {
     if (!req.user) return next(new ErrorHandler('Unauthorized', 401));
 
-    const todo = await Todo.findOne({ _id: req.params.id, owner: req.user._id });
+    const todo = await Todo.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
     if (!todo) return next(new ErrorHandler('Todo not found', 404));
 
-    todo.isCompleted = !todo.isCompleted;
+    const { title, description, isCompleted } = req.body;
+
+    if (title !== undefined) todo.title = title;
+    if (description !== undefined) todo.description = description;
+    if (isCompleted !== undefined) todo.isCompleted = isCompleted;
 
     await todo.save();
 
