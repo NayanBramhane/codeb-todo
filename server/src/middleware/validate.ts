fc0966264d6480825ba6210daa47297cd046470b
Nayan Bramhane <74374719+NayanBramhane@users.noreply.github.com>
2026-02-12 14:41:22 +0530

    corrected error message for express-validator

--- server/src/middleware/validate.ts
+++ server/src/middleware/validate.ts
@@ -5,9 +5,13 @@ export const validate = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
+    const firstErrorMessage =
+      errors.array()[0]?.msg || 'Unknown validation error';
+
     return res.status(400).json({
       success: false,
-      errors: errors.array().map((err) => err.msg),
+      // errors: errors.array().map((err) => err.msg),
+      message: firstErrorMessage,
     });
   }
 
