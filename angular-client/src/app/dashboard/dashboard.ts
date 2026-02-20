95b37073ef1708b02f7e50d485ac430977f4efd5
Nayan Bramhane <74374719+NayanBramhane@users.noreply.github.com>
2026-02-20 15:11:43 +0530

    UI for todo cards done

---
+++ angular-client/src/app/dashboard/dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Card } from '../card/card';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [Card],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class Dashboard {
+
+}
